a5d897b1e3d0bd259dd7ec171ab064e432b86576
Davis Ford <ford_davis@bah.com>
2017-01-06 21:56:26 -0500

    done

--- js/breakout.js
+++ js/breakout.js
@@ -3,12 +3,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
   run();
 });
 
+/*
+ Run using MAMP (OSX) or XAMPP (Win)
+ eg http://localhost:8888/phaser-first-project/
+ */
+
 var run = function () {
   var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
-  var ball, paddle;
+  var ball1, ball2, paddle;
   var bricks, newBrick, brickInfo;
+  var lives = 3;
   var score = 0;
-  var scoreText;
+  var scoreText, startButton, livesText, lifeLostText;
+  var playing = false;
+
 
   function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -20,27 +28,35 @@ var run = function () {
     game.load.image('paddle', 'img/paddle.png');
     game.load.image('brick', 'img/brick.png');
     game.load.spritesheet('ball', 'img/wobble.png', 20, 20);
+    game.load.spritesheet('button', 'img/button.png', 120, 40);
   }
+
 
   function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.checkCollision.down = false;
 
     // Give ball some physics
-    ball = game.add.sprite(game.world.width * 0.5, game.world.height - 25, 'ball');
-    ball.animations.add('wobble', [0, 1, 0, 2, 0, 1, 0, 2, 0], 24);
-    ball.anchor.set(0.5);
-    game.physics.enable(ball, Phaser.Physics.ARCADE);
-    ball.body.bounce.set(1);
-    ball.body.collideWorldBounds = true;
-    ball.body.velocity.set(150, -150);
+    ball1 = game.add.sprite(game.world.width * 0.5, game.world.height - 25, 'ball');
+    ball1.animations.add('wobble', [0, 1, 0, 2, 0, 1, 0, 2, 0], 24);
+    ball1.anchor.set(0.5);
+    game.physics.enable(ball1, Phaser.Physics.ARCADE);
+    ball1.body.bounce.set(1);
+    ball1.body.collideWorldBounds = true;
+
+    ball2 = game.add.sprite(game.world.width * 0.5 + 40, game.world.height - 45, 'ball');
+    ball2.animations.add('wobble', [0, 1, 0, 2, 0, 1, 0, 2, 0], 24);
+    ball2.anchor.set(0.5);
+    game.physics.enable(ball2, Phaser.Physics.ARCADE);
+    ball2.body.bounce.set(1);
+    ball2.body.collideWorldBounds = true;
 
     // Define ball limits
-    game.physics.arcade.checkCollision.down = false;
-    ball.checkWorldBounds = true;
-    ball.events.onOutOfBounds.add(function () {
-      console.log('Game Over!');
-      document.location.reload();
-    }, this);
+    ball1.checkWorldBounds = true;
+    ball1.events.onOutOfBounds.add(ballLeavesBounds, this);
+
+    ball2.checkWorldBounds = true;
+    ball2.events.onOutOfBounds.add(ballLeavesBounds, this);
 
 
     // Paddle and buttons
@@ -53,28 +69,63 @@ var run = function () {
     initBricks();
 
     // Score
-    scoreText = game.add.text(5, 5, 'Points: ' + score, {font: '18px Arial', fill: '#0095DD'});
+    var textStyle = {font: '18px Arial', fill: '#0095DD'};
+    scoreText = game.add.text(5, 5, 'Points: ' + score, textStyle);
+    livesText = game.add.text(game.world.width - 5, 5, 'Lives: ' + lives, textStyle);
+    livesText.anchor.set(1, 0);
+    lifeLostText = game.add.text(game.world.width * 0.5, game.world.height * 0.5, 'Life lost, click to continue', textStyle);
+    lifeLostText.anchor.set(0.5);
+    lifeLostText.visible = false;
+
+    // Button
+    startButton = game.add.button(game.world.width * 0.5, game.world.height * 0.5, 'button', startGame, this, 1, 0, 2);
+    startButton.anchor.set(0.5);
+  }
+
+  function ballLeavesBounds() {
+    lives--;
+    if (lives) {
+      livesText.setText('Lives: ' + lives);
+      lifeLostText.visible = true;
+      ball1.reset(game.world.width * 0.5, game.world.height - 25);
+      ball2.reset(game.world.width * 0.5 + 40, game.world.height - 45);
+      paddle.reset(game.world.width * 0.5, game.world.height - 5);
+      game.input.onDown.addOnce(function () {
+        lifeLostText.visible = false;
+        ball1.body.velocity.set(150, -150);
+        ball2.body.velocity.set(140, -140);
+      }, this);
+    }
+    else {
+      alert('You lost, game over!');
+      document.location.reload();
+    }
   }
 
   function update() {
-    game.physics.arcade.collide(ball, paddle, ballHitPaddle);
-    game.physics.arcade.collide(ball, bricks, ballHitBrick);
-    paddle.x = game.input.x || game.world.width * 0.5;
+    game.physics.arcade.collide(ball1, paddle, ballHitPaddle);
+    game.physics.arcade.collide(ball2, paddle, ballHitPaddle);
+    game.physics.arcade.collide(ball1, bricks, ballHitBrick);
+    game.physics.arcade.collide(ball2, bricks, ballHitBrick);
+    game.physics.arcade.collide(ball1, ball2, ballHitBall);
+    if (playing) {
+      paddle.x = game.input.x || game.world.width * 0.5;
+    }
+  }
 
+  function startGame() {
+    startButton.destroy();
+    ball1.body.velocity.set(150, -150);
+    ball2.body.velocity.set(140, -140);
+    playing = true;
   }
 
   function initBricks() {
     brickInfo = {
       width: 50,
       height: 20,
-      count: {
-        row: 7,
-        col: 3
-      },
-      offset: {
-        top: 50,
-        left: 60
-      },
+      count: {row: 7, col: 3},
+      offset: {top: 50, left: 60},
       padding: 10
     };
     bricks = game.add.group();
@@ -95,32 +146,34 @@ var run = function () {
     ball.animations.play('wobble');
   }
 
+  function ballHitBall(ball1, ball2) {
+    ball1.animations.play('wobble');
+    ball2.animations.play('wobble');
+  }
+
   function ballHitBrick(ball, brick) {
     var killTween = game.add.tween(brick.scale);
     killTween.to({x: 0, y: 0}, 200, Phaser.Easing.Linear.None);
     killTween.onComplete.addOnce(function () {
       brick.kill();
-    }, this);
+      checkGameWinCondition();
+    });
     killTween.start();
     score += 10;
     scoreText.setText('Points: ' + score);
-    checkGameWinCondition();
   }
 
   function checkGameWinCondition() {
-    var count_alive = 0;
     var num_children = bricks.children.length;
+    var count_alive = 0;
     for (var i = 0; i < num_children; i++) {
       if (bricks.children[i].alive) {
         count_alive++;
       }
     }
     if (count_alive === 0) {
-      alert('You win!');
+      alert.log('You win! Score:' + score);
       document.location.reload();
     }
   }
-
-  //TODO: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_breakout_game_Phaser/Buttons
-
 };
